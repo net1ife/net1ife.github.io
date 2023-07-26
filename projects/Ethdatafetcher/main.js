73f502f1dbafbcded766d7c9262ccf2f29d52148
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-26 02:08:51 -0500

    Create main.js

---
+++ projects/Ethdatafetcher/main.js
@@ -0,0 +1,62 @@
+const Web3 = window.Web3;
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${localStorage.getItem('INFURA_PROJECT_ID')}`));
+
+const contentDiv = document.getElementById('content');
+
+async function getBlockDetails(blockNumber) {
+    try {
+        return await web3.eth.getBlock(blockNumber);
+    } catch (error) {
+        console.log(`Failed to fetch block ${blockNumber}`);
+        return null;
+    }
+}
+
+async function getTransactionDetails(tx) {
+    try {
+        return await web3.eth.getTransaction(tx);
+    } catch (error) {
+        console.log(`Failed to fetch transaction ${tx}`);
+        return null;
+    }
+}
+
+function printTransactionDetails(txDetails, idx) {
+    if (txDetails.to) {
+        contentDiv.innerHTML += `<p>Transaction ${idx}: ${txDetails.hash} <br>From: ${txDetails.from} <br>To: ${txDetails.to} <br>Amount: ${web3.utils.fromWei(txDetails.value, 'ether')} Ether</p>`;
+    } else {
+        contentDiv.innerHTML += `<p>Contract creation transaction ${idx}: ${txDetails.hash} <br>From: ${txDetails.from}</p>`;
+    }
+}
+
+async function printBlockDetails(latestBlock, i) {
+    let block = await getBlockDetails(latestBlock - i);
+    if (block === null) {
+        return;
+    }
+
+    contentDiv.innerHTML += `<h2>Block ${i+1} (Block Number: ${block.number})</h2>`;
+    contentDiv.innerHTML += `<p>Number of transactions: ${block.transactions.length}</p>`;
+
+    for (let idx = 0; idx < block.transactions.length; idx++) {
+        let txDetails = await getTransactionDetails(block.transactions[idx]);
+        if (txDetails === null) {
+            continue;
+        }
+        printTransactionDetails(txDetails, idx + 1);
+    }
+}
+
+window.onload = async function main() {
+    let latestBlock;
+    try {
+        latestBlock = await web3.eth.getBlockNumber();
+    } catch (error) {
+        console.log("Failed to connect to the Ethereum network");
+        return;
+    }
+
+    for (let i = 0; i < 10; i++) {
+        await printBlockDetails(latestBlock, i);
+    }
+}
