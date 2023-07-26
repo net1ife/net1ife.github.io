fa5a58b6448aeb2a5821fd04fb047ebdf1a7c030
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-26 17:28:55 -0500

    Update sudoku.js

--- projects/Sudoku/sudoku.js
+++ projects/Sudoku/sudoku.js
@@ -1,9 +1,7 @@
 let boardElement = document.getElementById('sudoku-board');
 let newGameButton = document.getElementById('new-game');
-let scoreElement = document.getElementById('score');
 
 let board = [...Array(9)].map(e => Array(9).fill(0));
-let solution = [...Array(9)].map(e => Array(9).fill(0));
 
 function drawBoard() {
     boardElement.innerHTML = '';
@@ -23,9 +21,8 @@ function checkValidity(e, i, j) {
     let value = e.target.value;
     if (value === '') return;
     let num = parseInt(value);
-    if (!Number.isInteger(num) || num < 1 || num > 9 || num !== solution[i][j]) {
+    if (!Number.isInteger(num) || num < 1 || num > 9 || num !== board[i][j]) {
         e.target.classList.add('error');
-        scoreElement.textContent = parseInt(scoreElement.textContent) + 1;
     } else {
         e.target.classList.remove('error');
     }
@@ -36,16 +33,11 @@ function generatePuzzle() {
         for(let j = 0; j < 9; j++) {
             let num = Math.ceil(Math.random() * 9);
             board[i][j] = Math.random() < 0.2 ? num : 0;
-            solution[i][j] = num;
         }
     }
     drawBoard();
 }
 
-newGameButton.addEventListener('click', () => {
-    scoreElement.textContent = '0';
-    generatePuzzle();
-});
+newGameButton.addEventListener('click', generatePuzzle);
 
 generatePuzzle();
-
