91eed777ca6e05ac6a647090d6c4bed4f7978604
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-26 02:39:32 -0500

    Update main.js

--- projects/Ethdatafetcher/main.js
+++ projects/Ethdatafetcher/main.js
@@ -1,61 +1,67 @@
+const Web3 = window.Web3;
 const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/f38ec3cfb102401da04f4e0fbcb041f2'));
 
-document.getElementById("blockForm").addEventListener('submit', (event) => {
-    event.preventDefault();
-    let blockNumber = document.getElementById("blockNumber").value;
-    getBlocks(blockNumber);
-});
+const contentDiv = document.getElementById('content');
+const blockForm = document.getElementById('blockForm');
+const blockNumber = document.getElementById('blockNumber');
 
-const getBlocks = async (blockNumber) => {
-    let content = document.getElementById('content');
-    content.innerHTML = '';  // Clear previous results
-
-    // Get the latest block number
-    const latestBlock = await web3.eth.getBlockNumber();
-
-    // Fetch the requested number of blocks
-    for (let i = 0; i < blockNumber; i++) {
-        const block = await web3.eth.getBlock(latestBlock - i);
-
-        // Create the block panel
-        let blockPanel = document.createElement('div');
-        blockPanel.classList.add('block-panel');
-        blockPanel.innerHTML = `
-            <h2>Block ${i + 1} (Block Number: ${block.number})</h2>
-            <div class="block-details">
-                <p>Number of transactions: ${block.transactions.length}</p>
-            </div>
-        `;
-
-        // Fetch transactions of the block
-        for (let j = 0; j < block.transactions.length; j++) {
-            const tx = await web3.eth.getTransaction(block.transactions[j]);
-
-            // Append transaction to the block panel
-            let txDetails = document.createElement('p');
-            if (tx.to) {
-                txDetails.innerHTML = `
-                    Transaction ${j + 1}: ${tx.hash} <br>
-                    From: ${tx.from} <br>
-                    To: ${tx.to} <br>
-                    Amount: ${web3.utils.fromWei(tx.value, 'ether')} Ether
-                `;
-            } else {
-                txDetails.innerHTML = `
-                    Contract creation transaction ${j + 1}: ${tx.hash} <br>
-                    From: ${tx.from}
-                `;
-            }
-            blockPanel.querySelector('.block-details').appendChild(txDetails);
-        }
-
-        // Append the block panel to the content div
-        content.appendChild(blockPanel);
-
-        // Add click event to toggle block details
-        blockPanel.querySelector('h2').addEventListener('click', (event) => {
-            let blockDetails = event.target.nextElementSibling;
-            blockDetails.style.display = blockDetails.style.display === "none" ? "block" : "none";
-        });
+async function getBlockDetails(blockNumber) {
+    try {
+        return await web3.eth.getBlock(blockNumber);
+    } catch (error) {
+        console.log(`Failed to fetch block ${blockNumber}`);
+        return null;
     }
-};
+}
+
+async function getTransactionDetails(tx) {
+    try {
+        return await web3.eth.getTransaction(tx);
+    } catch (error) {
+        console.log(`Failed to fetch transaction ${tx}`);
+        return null;
+    }
+}
+
+function printTransactionDetails(txDetails, idx) {
+    if (txDetails['to']) {
+        contentDiv.innerHTML += `<p>Transaction ${idx}: ${txDetails['hash']} <br>From: ${txDetails['from']} <br>To: ${txDetails['to']} <br>Amount: ${web3.utils.fromWei(txDetails['value'], 'ether')} Ether</p>`;
+    } else {
+        contentDiv.innerHTML += `<p>Contract creation transaction ${idx}: ${txDetails['hash']} <br>From: ${txDetails['from']}</p>`;
+    }
+}
+
+async function printBlockDetails(latestBlock, i) {
+    const block = await getBlockDetails(latestBlock - i);
+    if (block == null) return;
+
+    contentDiv.innerHTML += `<h2>Block ${i+1} (Block Number: ${block.number})</h2>`;
+    contentDiv.innerHTML += `<p>Number of transactions: ${block.transactions.length}</p>`;
+
+    for (let idx = 0; idx < block.transactions.length; idx++) {
+        const txDetails = await getTransactionDetails(block.transactions[idx]);
+        if (txDetails == null) continue;
+        printTransactionDetails(txDetails, idx+1);
+    }
+}
+
+async function printBlocks(numBlocks) {
+    let latestBlock;
+    try {
+        latestBlock = await web3.eth.getBlockNumber();
+    } catch (error) {
+        console.log("Failed to connect to the Ethereum network");
+        return;
+    }
+
+    for (let i = 0; i < numBlocks; i++) {
+        await printBlockDetails(latestBlock, i);
+    }
+}
+
+blockForm.onsubmit = function(e) {
+    e.preventDefault();  // Prevent the form from refreshing the page
+    contentDiv.innerHTML = "";  // Clear the content
+    printBlocks(blockNumber.value);  // Print the requested number of blocks
+}
+
