8c19fb327052cea1871010bd9b31a51bb28bd887
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-26 02:07:46 -0500

    Create Ethdatafetcher.py

---
+++ projects/Ethdatafetcher/Ethdatafetcher.py
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <script src="https://cdn.jsdelivr.net/npm/web3@1.3.4/dist/web3.min.js"></script>
+</head>
+<body>
+    <header>
+        <h1>Ethereum Block Explorer</h1>
+    </header>
+
+    <main>
+        <div id="content"></div>
+    </main>
+
+    <script src="main.js"></script>
+</body>
+</html>
