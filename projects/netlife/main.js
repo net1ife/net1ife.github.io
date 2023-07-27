e8d15cb36e84cb40066215eb35b521c4c39daa8c
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-26 23:11:07 -0500

    Update main.js

--- projects/netlife/main.js
+++ projects/netlife/main.js
@@ -1,117 +1,86 @@
-// Import TensorFlow.js
 import * as tf from '@tensorflow/tfjs';
 
-// Set up constants
-const WIDTH = 400;
-const HEIGHT = 400;
-const CELL_SIZE = 10;
-const GRID_WIDTH = WIDTH / CELL_SIZE;
-const GRID_HEIGHT = HEIGHT / CELL_SIZE;
-const BREED_THRESHOLD = 0.75;
-const AGE_LIMIT = 100;
+const WIDTH = 400, HEIGHT = 400, CELL_SIZE = 10;
+const GRID_WIDTH = WIDTH / CELL_SIZE, GRID_HEIGHT = HEIGHT / CELL_SIZE;
+const BREED_THRESHOLD = 0.75, AGE_LIMIT = 100;
 
-// Set up the canvas and context
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-
-// Get control elements
-const resetButton = document.getElementById('resetButton');
-const delayRange = document.getElementById('delayRange');
-
-// Set up the cells
 let cells = [];
-for (let i = 0; i < GRID_HEIGHT; i++) {
-    cells[i] = [];
-    for (let j = 0; j < GRID_WIDTH; j++) {
-        cells[i][j] = createCell();
+
+class Cell {
+    constructor() {
+        this.neuralNet = tf.sequential();
+        this.neuralNet.add(tf.layers.dense({units: 18, inputShape: [9], activation: 'relu'}));
+        this.neuralNet.add(tf.layers.dense({units: 9, activation: 'relu'}));
+        this.neuralNet.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
+        this.neuralNet.compile({loss: 'meanSquaredError', optimizer: 'adam'});
+        this.state = Math.random();
+        this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        this.age = 0;
+    }
+
+    async predictNextState(neighborhoodStates) {
+        const targetState = tf.tensor1d([neighborhoodStates.reduce((a, b) => a + b, 0) / neighborhoodStates.length]);
+        const predictedState = this.neuralNet.predict(tf.tensor2d([neighborhoodStates])).dataSync()[0];
+        const loss = (predictedState - targetState.arraySync()[0]) ** 2;
+        await this.neuralNet.fit(tf.tensor2d([neighborhoodStates]), targetState, {epochs: 1});
+        this.state = predictedState;
+        this.age++;
+        if (this.age > AGE_LIMIT) this.reset();
+    }
+
+    breed(other) {
+        const child = new Cell();
+        const weights = this.neuralNet.getWeights();
+        const otherWeights = other.neuralNet.getWeights();
+        const newWeights = weights.map((w, i) => tf.add(w, otherWeights[i]).div(2.0));
+        child.neuralNet.setWeights(newWeights);
+        return child;
+    }
+
+    reset() {
+        this.state = 0;
+        this.age = 0;
     }
 }
 
-// Function to create a new cell
-function createCell() {
-    // Define the model architecture
-    const model = tf.sequential();
-    model.add(tf.layers.dense({inputShape: [9], units: 18, activation: 'relu'}));
-    model.add(tf.layers.dense({units: 9, activation: 'relu'}));
-    model.add(tf.layers.dense({units: 1, activation: 'sigmoid'}));
-
-    // Compile the model
-    model.compile({loss: 'meanSquaredError', optimizer: 'adam'});
-
-    // Return the new cell
-    return {
-        model: model,
-        state: Math.random(),
-        age: 0
-    };
-}
-
-// Function to get the neighborhood of a cell
-function getNeighborhood(x, y) {
-    const neighborhood = [];
-    for (let i = -1; i <= 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-            const nx = (x + i + GRID_HEIGHT) % GRID_HEIGHT;
-            const ny = (y + j + GRID_WIDTH) % GRID_WIDTH;
-            neighborhood.push(cells[nx][ny].state);
+function getNeighborhood(i, j) {
+    let neighborhood = [];
+    for (let x = i - 1; x <= i + 1; x++) {
+        for (let y = j - 1; y <= j + 1; y++) {
+            neighborhood.push(cells[x < 0 ? GRID_HEIGHT - 1 : x >= GRID_HEIGHT ? 0 : x][y < 0 ? GRID_WIDTH - 1 : y >= GRID_WIDTH ? 0 : y].state);
         }
     }
-    return tf.tensor(neighborhood, [1, 9]);
+    return neighborhood;
 }
 
-// Function to update the state of the game
-function updateState() {
-    // Create new cells
-    const newCells = [];
-
-    // Go through the cells
+async function update() {
     for (let i = 0; i < GRID_HEIGHT; i++) {
         for (let j = 0; j < GRID_WIDTH; j++) {
-            // Get the neighborhood of the cell
             const neighborhood = getNeighborhood(i, j);
-
-            // Predict the next state of the cell
-            const nextState = cells[i][j].model.predict(neighborhood).dataSync()[0];
-            cells[i][j].state = nextState;
-
-            // Increase the age of the cell
-            cells[i][j].age++;
-
-            // Reset the cell if it's too old
-            if (cells[i][j].age > AGE_LIMIT) {
-                cells[i][j] = createCell();
-            }
-
-            // Breed the cell if it's state is high enough
-            if (nextState > BREED_THRESHOLD) {
-                const neighbors = neighborhood.dataSync();
-                for (const neighbor of neighbors) {
-                    if (neighbor > BREED_THRESHOLD) {
-                        const child = createCell();
-                        child.model.setWeights(tf.add(cells[i][j].model.getWeights(), child.model.getWeights()));
-                        newCells.push(child);
+            await cells[i][j].predictNextState(neighborhood);
+            if (cells[i][j].state > BREED_THRESHOLD) {
+                for (let n = 0; n < neighborhood.length; n++) {
+                    if (neighborhood[n] > BREED_THRESHOLD) {
+                        cells[Math.floor(Math.random() * GRID_HEIGHT)][Math.floor(Math.random() * GRID_WIDTH)] = cells[i][j].breed(cells[(i + Math.floor(n / 3) - 1) % GRID_HEIGHT][(j + n % 3 - 1) % GRID_WIDTH]);
                     }
                 }
             }
         }
     }
-
-    // Add the new cells to the game
-    for (const cell of newCells) {
-        const x = Math.floor(Math.random() * GRID_HEIGHT);
-        const y = Math.floor(Math.random() * GRID_WIDTH);
-        cells[x][y] = cell;
-    }
 }
 
 // Function to draw the game
 function draw() {
+    const canvas = document.getElementById('game');
+    const ctx = canvas.getContext('2d');
+
     // Clear the canvas
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
 
     // Draw the cells
     for (let i = 0; i < GRID_HEIGHT; i++) {
         for (let j = 0; j < GRID_WIDTH; j++) {
+            // Change color based on the cell's state
             if (cells[i][j].state > 0.5) {
                 ctx.fillStyle = '#50fa7b';
             } else {
@@ -122,30 +91,21 @@ function draw() {
     }
 }
 
-// Function to run the game loop
-function gameLoop() {
-    // Update the state
-    updateState();
-
-    // Draw the game
-    draw();
-
-    // Schedule the next game loop
-    setTimeout(gameLoop, delayRange.value);
+// Create the initial grid
+for (let i = 0; i < GRID_HEIGHT; i++) {
+    let row = [];
+    for (let j = 0; j < GRID_WIDTH; j++) {
+        row.push(new Cell());
+    }
+    cells.push(row);
 }
 
-// Event listener for the reset button
-resetButton.addEventListener('click', () => {
-    // Reset the cells
-    for (let i = 0; i < GRID_HEIGHT; i++) {
-        for (let j = 0; j < GRID_WIDTH; j++) {
-            cells[i][j] = createCell();
-        }
-    }
-
-    // Redraw the game
+// Set up the game loop
+async function gameLoop() {
     draw();
-});
+    await update();
+    requestAnimationFrame(gameLoop);
+}
 
 // Start the game loop
 gameLoop();
