957e45306cbed5f916e79b2b098a0480edcc6d87
net1ife <127271713+net1ife@users.noreply.github.com>
2023-07-26 19:18:01 -0500

    Update sudoku.js

--- projects/Sudoku/sudoku.js
+++ projects/Sudoku/sudoku.js
@@ -17,15 +17,33 @@ function drawBoard() {
     }
 }
 
-function checkValidity(e, i, j) {
+function checkValidity(e, row, col) {
     let value = e.target.value;
     if (value === '') return;
     let num = parseInt(value);
-    if (!Number.isInteger(num) || num < 1 || num > 9 || num !== board[i][j]) {
+    if (!Number.isInteger(num) || num < 1 || num > 9 || !isValid(num, row, col)) {
         e.target.classList.add('error');
     } else {
         e.target.classList.remove('error');
     }
+}
+
+function isValid(num, row, col) {
+    for(let i = 0; i < 9; i++) {
+        if(board[i][col] === num || board[row][i] === num) {
+            return false;
+        }
+    }
+    let startRow = row - row % 3;
+    let startCol = col - col % 3;
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++) {
+            if(board[i + startRow][j + startCol] === num) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 function generatePuzzle() {
